263174e6c3752fb54fa31d3a50787897f195a8a7
CodeDragon82 <michaelrichar280@outlook.com>
2022-05-11 21:07:52 +0100

    Update ControllerListener.java

--- ControllerListener.java
+++ ControllerListener.java
@@ -84,7 +84,13 @@ public class ControllerListener extends Thread {
 
                     Message.success("reconnected to controller", 0);
                 } catch (IOException e1) { 
-                    Message.failed("failed to reconnect to controller", 0); 
+                    Message.failed("failed to reconnect to controller", 0);
+                    
+                    try {
+                        Thread.sleep(Dstore.getTimeout());
+                    } catch (InterruptedException e2) {
+                        e2.printStackTrace();
+                    }
                 }
             } catch (IOException e) {
                 Message.error("controller listener crashed: " + e.getMessage(), 0);
